9f6895155789c9c08d5b15cf5acb9dc62d11185b
Alex <alex.jolas40@gmail.com>
2023-12-18 01:06:34 +0100

    TP3

--- tp3cucumber/my-vue-app/features/exports.feature
+++ tp3cucumber/my-vue-app/features/exports.feature
@@ -1,6 +1,9 @@
 Feature: Checking for the "export default" statement in React files
 
-  Scenario: Verify the presence of "export default" in a React file
+  Scenario Outline: Verify the presence of "export default" in a React file
     Given I want to ensure that my React files have a default export
     When I check the React file "<file_name>"
-    Then I should find the "export default" statement
+    Then I should find the "<statement>" statement
+    Examples:
+      | file_name | statement |
+      | index.tsx | export default |
--- tp3cucumber/my-vue-app/step_definitions/export_steps.ts
+++ tp3cucumber/my-vue-app/step_definitions/export_steps.ts
@@ -1,18 +1,20 @@
-import { Given, When, Then } from 'cucumber';
+
 import * as fs from 'fs';
+import {Given, Then, When} from "@cucumber/cucumber";
 
 let fileContent: string;
 
 Given('I want to ensure that my React files have a default export', function () {
     // You can add configurations or initializations here if needed
+    return 'pending'
 });
 
 When('I check the React file {string}', function (fileName: string) {
     fileContent = fs.readFileSync(fileName, 'utf8');
 });
 
-Then('I should find the "export default" statement', function () {
-    const hasExportDefault = fileContent.includes('export default');
+Then('I should find the {string} statement', function (statement: string) {
+    const hasExportDefault = fileContent.includes(statement);
     if (!hasExportDefault) {
         throw new Error('The "export default" statement was not found in the file.');
     }
